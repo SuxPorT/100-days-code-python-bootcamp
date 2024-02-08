1afbc0e0905d3ceb422b0ceb941b76be62920db2
Alexys Santiago <SuxPorT@hotmail.com>
2024-02-07 22:48:59 -0300

    feat: add Day #01 - Beginner

---
+++ day-01-beginner/02-debugging-practice.py
@@ -0,0 +1,17 @@
+# Fix the code below 👇
+
+# Answer part 1. Missing double quotes before the word Day.
+# print(Day 1 - String Manipulation")
+print("Day 1 - String Manipulation")
+
+# Answer part 2. Outer double quotes changed to single quotes.
+# print("String Concatenation is done with the "+" sign.")
+print('String Concatenation is done with the "+" sign.')
+
+# Answer part 3. Extra indentation removed
+#   print('e.g. print("Hello " + "world")')
+print('e.g. print("Hello " + "world")')
+
+# Answer part 4. Extra ( in print function removed.
+# print(("New lines can be created with a backslash and n.")
+print("New lines can be created with a backslash and n.")
---
+++ projects/day-01-project.py
@@ -0,0 +1,6 @@
+# Day 1 Project: Band New Generator
+
+print("Welcome to the Band Name Generator.")
+street = input("What's the name of the city you grew up in?\n")
+pet = input("\nWhat's your pet's name?\n")
+print("\nYour band name could be " + street + " " + pet)
